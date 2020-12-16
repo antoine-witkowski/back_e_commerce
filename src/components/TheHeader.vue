f19af48c46da1a5e939b5e9492373421c0d9aa08
Wins <tktmmpaas@gmail.com>
2020-12-16 23:28:31 +0100

    implementation header

---
+++ src/components/TheHeader.vue
@@ -0,0 +1,30 @@
+<template>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <a class="navbar-brand">
+      POKEMON
+    </a>
+    <button class="navbar-toggler">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse">
+      <ul class="navbar-nav">
+        <li class="nav-item">
+          <a class="nav-link">Boutique</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link">Admin</a>
+        </li>
+      </ul>
+
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+}
+</script>
+
+<style>
+
+</style>
