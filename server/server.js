5d82f1c81bd0506162e21f2dcf6c5b07e317336a
Witkowski Antoine <witkowski.antoine@gmail.com>
2020-12-16 20:31:54 +0100

    :building_construction: change structure code client/server

---
+++ server/server.js
@@ -0,0 +1,35 @@
+
+let express = require('express');
+let path = require('path');
+let bodyParser = require("body-parser");
+
+let app = express();
+
+//app.set('view engine', 'ejs');
+
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+ 
+app.get('/', (request, response) => {
+  response.sendFile(path.join(__dirname + '/views_temporary/index.html'));
+});
+
+/*
+app.post('/', (request, response) => {
+  let name = request.body.name; 
+  if (name === undefined || name === '') {
+    response.render('pages/index', { message:"Vous n'avez pas entré de nom" });
+  } else {
+    response.render('pages/index', { message: name + " a bien été enregistré" });
+  }
+  response.redirect('/');
+});
+*/
+/*
+app.get('/', (request, response) => {
+    response.render('src/views/index.vue');
+});
+*/
+app.listen(8090);
