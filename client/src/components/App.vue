d89e6b625f87cab276801de762349d2222e49afd
Witkowski Antoine <witkowski.antoine@gmail.com>
2020-12-17 09:47:59 +0100

    update client folder

---
+++ client/src/components/App.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    <the-header></the-header>
+  </div>
+</template>
+
+<script>
+import TheHeader from './components/TheHeader.vue';
+
+export default {
+  name: 'App',
+  components: {
+    TheHeader
+    
+  }
+}
+</script>
+
+<style>
+
+</style>
